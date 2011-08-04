78dd52e059e86bd37a53684c7c3470847f419ab8
Damien Bourdette <dbourdette@gmail.com>
2011-08-04 17:39:00 +0200

    Delete events : display message that capped collection can not be cleared

--- src/main/java/org/otto/web/controller/EventsController.java
+++ src/main/java/org/otto/web/controller/EventsController.java
@@ -1,9 +1,5 @@
 package org.otto.web.controller;
 
-import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.joda.time.DateTime;
 import org.otto.event.DBSource;
 import org.otto.event.Event;
@@ -15,54 +11,68 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/sources/{name}/events")
 public class EventsController {
 
-	@Inject
-	private Sources sources;
-    
+    @Inject
+    private Sources sources;
+
     @Inject
     private FlashScope flashScope;
 
-	@RequestMapping
-	public String events(@PathVariable String name, Model model) {
-		DBSource source = sources.getSource(name);
-		
-		model.addAttribute("navItem", "logs");
-		model.addAttribute("events", source.findEvents(100));
+    @RequestMapping
+    public String events(@PathVariable String name, Model model) {
+        DBSource source = sources.getSource(name);
 
-		return "sources/events";
-	}
+        model.addAttribute("navItem", "logs");
+        model.addAttribute("events", source.findEvents(100));
 
-	@RequestMapping("/delete")
-	public String clearForm(@PathVariable String name, Model model) {
-		model.addAttribute("navItem", "logs");
+        return "sources/events";
+    }
 
-		return "sources/events_delete_form";
-	}
+    @RequestMapping("/delete")
+    public String clearForm(@PathVariable String name, Model model) {
+        model.addAttribute("navItem", "logs");
 
-	@RequestMapping(method = RequestMethod.DELETE)
-	public String clear(@PathVariable String name) {
-		DBSource source = sources.getSource(name);
-		
-		source.clearEvents();
-		
-		flashScope.message("Events have been cleared for source " + name);
+        DBSource source = sources.getSource(name);
 
-		return "redirect:/sources/{name}/events";
-	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public void post(@PathVariable String name, HttpServletRequest request, HttpServletResponse response) {
-		@SuppressWarnings("unchecked")
-		Event event = Event.fromMap(request.getParameterMap());
-		
-		event.setDateIfNoneDefined(new DateTime());
+        if (source.isCapped()) {
+            return "sources/events_delete_capped";
+        } else {
+            return "sources/events_delete_form";
+        }
+    }
 
-		sources.getSource(name).post(event);
-		
-		response.setStatus(200);
-	}
+    @RequestMapping(method = RequestMethod.DELETE)
+    public String clear(@PathVariable String name) {
+        DBSource source = sources.getSource(name);
+
+        if (source.isCapped()) {
+            return "redirect:/sources/{name}/events";
+        }
+
+        source.clearEvents();
+
+        flashScope.message("Events have been cleared for source " + name);
+
+        return "redirect:/sources/{name}/events";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void post(@PathVariable String name, HttpServletRequest request, HttpServletResponse response) {
+        @SuppressWarnings("unchecked")
+        Event event = Event.fromMap(request.getParameterMap());
+
+        event.setDateIfNoneDefined(new DateTime());
+
+        sources.getSource(name).post(event);
+
+        response.setStatus(200);
+    }
 
 }
