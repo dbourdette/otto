6a07d168f6112569f5d923a19ede9ff98e952c0d
Damien Bourdette <damien.bourdette@rtlnet.fr>
2012-12-11 09:41:26 +0100

    removed ehcache

--- otto-web/src/main/java/com/github/dbourdette/otto/Registry.java
+++ otto-web/src/main/java/com/github/dbourdette/otto/Registry.java
@@ -2,9 +2,6 @@ package com.github.dbourdette.otto;
 
 import com.google.code.morphia.Datastore;
 import com.mongodb.DB;
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.config.CacheConfiguration;
 
 /**
  *
@@ -16,24 +13,5 @@ import net.sf.ehcache.config.CacheConfiguration;
 public class Registry {
     public static DB mongoDb;
 
-    public static Cache sourceCache;
-
     public static Datastore datastore;
-
-    static {
-        CacheManager cacheManager = new CacheManager();
-
-        CacheConfiguration config = new CacheConfiguration();
-        config.setName("sourceCache");
-        config.setTimeToIdleSeconds(60);
-        config.setTimeToLiveSeconds(10 * 60);
-        config.setMaxEntriesLocalHeap(1000);
-        config.setOverflowToDisk(false);
-
-        Cache sourceCache = new Cache(config);
-
-        cacheManager.addCache(sourceCache);
-
-        Registry.sourceCache = sourceCache;
-    }
 }
